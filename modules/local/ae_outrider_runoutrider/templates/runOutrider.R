4c93cc8274ae76ece86697c74a184d7ba4f2f342
Yun Wang <freedom_be@live.cn>
2025-05-12 14:22:59 +0200

    add ae_outrider_runoutrider

---
+++ modules/local/ae_outrider_runoutrider/templates/runOutrider.R
@@ -0,0 +1,81 @@
+#!/usr/bin/env Rscript
+#https://github.com/gagneurlab/drop/blob/master/drop/modules/aberrant-expression-pipeline/OUTRIDER/runOutrider.R
+
+#'---
+#' title: Filter Counts for OUTRIDER
+#' author: Michaela Mueller
+#' wb:
+#'  log:
+#'   - snakemake: '`sm str(tmp_dir / "AE" / "{annotation}" / "{dataset}" / "runOUTRIDER.Rds")`'
+#'  input:
+#'   - ods: '`sm cfg.getProcessedResultsDir() + 
+#'           "/aberrant_expression/{annotation}/outrider/{dataset}/ods_unfitted.Rds"`'
+#'  output:
+#'   - ods_fitted: '`sm cfg.getProcessedResultsDir() + 
+#'           "/aberrant_expression/{annotation}/outrider/{dataset}/ods_fitted.Rds"`'
+#'  type: script
+#'  threads: 30
+#'---
+
+#+ echo=F
+
+# Initialize the prefix from the module
+prefix <- ifelse('$task.ext.prefix' == 'null', '$meta.id', '$task.ext.prefix')
+
+suppressPackageStartupMessages({
+    library(OUTRIDER)
+    library(SummarizedExperiment)
+    library(ggplot2)
+    library(data.table)
+    library(dplyr)
+    library(magrittr)
+    library(tools)
+})
+
+ods <- readRDS("$ods")
+implementation <- params.ae_implementation
+mp <- params.ae_max_tested_dimension_proportion
+register(MulticoreParam(${task.cpus}))
+
+## subset filtered
+ods <- ods[mcols(ods)$passedFilter,] 
+
+# add gene ranges to rowData
+gr <- unlist(endoapply(rowRanges(ods), range))
+if(length(gr) > 0){
+    rd <- rowData(ods)
+    rowRanges(ods) <- gr
+    rowData(ods) <- rd
+}
+
+ods <- estimateSizeFactors(ods)
+
+## find optimal encoding dimension
+a <- 5 
+b <- min(ncol(ods), nrow(ods)) / mp   # N/3
+
+maxSteps <- 15
+if(mp < 4){
+    maxSteps <- 20
+}
+
+Nsteps <- min(maxSteps, b)   # Do at most 20 steps or N/3
+# Do unique in case 2 were repeated
+pars_q <- round(exp(seq(log(a),log(b),length.out = Nsteps))) %>% unique
+ods <- findEncodingDim(ods, params = pars_q, implementation = implementation)
+opt_q <- getBestQ(ods)
+
+## fit OUTRIDER
+# ods <- OUTRIDER(ods, implementation = implementation)
+message(date(), ": SizeFactor estimation ...")
+ods <- estimateSizeFactors(ods)
+message(date(), ": Controlling for confounders ...")
+implementation <- tolower(implementation)
+ods <- controlForConfounders(ods, q=opt_q, implementation=implementation)
+if(grepl("^(peer|pca)$", implementation)){
+    message(date(), ": Fitting the data ...")
+    ods <- fit(ods)
+}
+message("outrider fitting finished")
+
+saveRDS(ods, paste(prefix, ".ods_fitted.Rds", sep="")
